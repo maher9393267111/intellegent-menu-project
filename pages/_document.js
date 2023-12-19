6553fbe3a61b1c1eb37287816b07680120ab9ed3
maher9393267111 <89878167+maher9393267111@users.noreply.github.com>
2023-12-19 15:24:45 +0300

    add title and nicon

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,26 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="ar"  dir='rtl'>
-      <Head />
+      <Head >
+      <title>
+            
+          mega teach
+            </title>
+
+
+
+      <link rel="icon" href="/site-icon.png" />
+
+ <link
+  rel="apple-touch-icon"
+  sizes="180x180"
+  href="/site-icon.png"
+/>
+<link rel="icon" sizes="32x32" href="/site-icon.png" />
+<link rel="icon" sizes="16x16" href="/site-icon.png" /> 
+
+<meta property="og:title" content="megateach" />
+      </Head>
       <body>
         <Main />
         <NextScript />
