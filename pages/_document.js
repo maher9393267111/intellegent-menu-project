e1c17528df5df34dc0a84fd62ec688df5e01a2a9
maher9393267111 <89878167+maher9393267111@users.noreply.github.com>
2023-12-19 17:36:21 +0300

    add again

--- pages/_document.js
+++ pages/_document.js
@@ -1,48 +1,34 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="ar"  dir='rtl'>
-      <Head >
-      <title>
-            
-       Logic Tech
-            </title>
+    <Html lang="ar" dir="rtl">
+      <Head>
+        <title>Logic Tech</title>
 
+        <link rel="icon" href="/site-icon.png" />
+        <link rel="preconnect" href="https://www.logictech.pro" />
 
+        <link rel="apple-touch-icon" sizes="180x180" href="/site-icon.png" />
+        <link rel="icon" sizes="32x32" href="/site-icon.png" />
+        <link rel="icon" sizes="16x16" href="/site-icon.png" />
 
-      <link rel="icon" href="/site-icon.png" />
-      <link rel="preconnect" href="https://www.logictech.pro" />
+        <meta property="og:title" content="logictech" />
 
- <link
-  rel="apple-touch-icon"
-  sizes="180x180"
-  href="/site-icon.png"
-/>
-<link rel="icon" sizes="32x32" href="/site-icon.png" />
-<link rel="icon" sizes="16x16" href="/site-icon.png" /> 
+        <meta name="keywords" content="logic, Tech" />
+        <meta name="author" content="logictech" />
+        <link rel="canonical" href="https://www.logictech.pro" />
+        {/* <!-- Add Open Graph tags for social media sharing --> */}
+        <meta property="og:title" content="logictech" />
+        <meta property="og:description" content="logictech logic tech" />
 
-<meta property="og:title" content="logictech" />
-
-<meta name="keywords" content="logic, Tech" />
-          <meta name="author" content="logictech" />
-          <link rel="canonical" href="https://itec-it.vercel.app/" />
-          {/* <!-- Add Open Graph tags for social media sharing --> */}
-          <meta property="og:title" content="logictech" />
-          <meta property="og:description" content="logictech logic tech" />
-          
-          <meta property="og:url" content="https://www.logictech.pro" />
-          <meta property="og:type" content="website" />
-          
-      
-
-
-
+        <meta property="og:url" content="https://www.logictech.pro" />
+        <meta property="og:type" content="website" />
       </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
